215c797da7250714f9aa9570f804a74171d899d1
Giordano Lins <giordano.lins@gmail.com>
2022-01-18 21:10:13 -0500

    changet port to process.env.PORT

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const PORT = 80;
+const PORT = process.env.PORT || 3001;
 const app = (0, express_1.default)();
 app.get('/bar-hello', (_, res) => {
     res.send('Hello from Bar!');
